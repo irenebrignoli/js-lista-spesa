987930747c737f7ec9e7d52ccdbd5bf588f7c7f9
Irene Brignoli <ire.brignoli@gmail.com>
2023-03-01 17:14:47 +0100

    Esercizio finito

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const listaSpesa = [
+const shopList = [
 
   'pane',
   'pomodori',
@@ -11,12 +11,63 @@ const listaSpesa = [
 
 ];
 
+const listDom = document.querySelector('ul');
 
 let i = 0;
 
-while ( i < listaSpesa.length - 1){
+let newListItem;
 
+while ( i < shopList.length ){
+
+  const listItem = `<li class="item">${shopList[i]}</li>`;
+  listDom.innerHTML += listItem;
+
+  newListItem = listItem;
   
-
   i ++;
 }
+
+const addListDom = document.getElementById('add-list');
+
+addListDom.addEventListener('click',
+
+  function(){
+
+    const newItem = prompt('Aggiungi elemento alla lista');
+
+    shopList.push(newItem);
+
+    console.log(shopList);
+
+    newListItem.innerHTML += newItem;
+
+    
+  }
+
+)
+
+
+/*
+
+let listaCompleta = false;
+
+const itemDaInserire = prompt('Aggiungi elemento alla lista');
+
+
+for (let c = 0; c < listaSpesa.length; c++){
+
+  const currentItem = listaSpesa[c];
+
+  if (currentItem == itemDaInserire){
+    listaCompleta = true;
+}
+
+}
+
+if (listaCompleta){
+  console.log('lista completa');
+}else{
+  listaSpesa.push(itemDaInserire);
+}
+
+*/
