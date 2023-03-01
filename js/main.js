9bd78e38f1b1ca9f1281405c427424baee68422b
Irene Brignoli <ire.brignoli@gmail.com>
2023-03-01 14:30:36 +0100

    Prima init

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const listaSpesa = [
+
+  'pane',
+  'pomodori',
+  'zucchine',
+  'cavolfiori',
+  'prosciutto',
+  'mais',
+  'zucca',
+  'carote'
+
+];
+
+
+let i = 0;
+
+while ( i < listaSpesa.length - 1){
+
+  
+
+  i ++;
+}
